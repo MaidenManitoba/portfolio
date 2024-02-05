9725cf3a13317111b5b07a91cf906d68b7a45ac0
Shann <shan.cee.mb204@gmail.com>
2024-02-05 18:11:19 -0500

    added spacing to footer text, added js for nav bar, adjusted wrappers

---
+++ script.js
@@ -0,0 +1,7 @@
+const burgerButton = document.getElementById("hamburger");
+const menu = document.getElementById("nav-menu");
+const navLinks = document.querySelector(".nav-link");
+
+
+hamburger.addEventListener("click", mobileMenu);
+
