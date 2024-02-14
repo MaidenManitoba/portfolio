c81b6457b3ea965c0b0b267eadbfc813247e38d4
Shann <shan.cee.mb204@gmail.com>
2024-02-14 16:31:48 -0500

    added modal to html, css and js

--- script.js
+++ script.js
@@ -11,3 +11,37 @@ burgerButton.addEventListener("click", function(){
     console.log(menu);
 });
 
+
+// modal variables
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay-read-me");
+const openModalBtn = document.querySelector(".btn-read-me");
+const closeModalBtn = document.querySelector(".btn-close");
+const body = document.body;
+
+// Function for opening the modal
+const openModal = function () {
+	modal.classList.remove("hidden");
+	overlay.classList.remove("hidden");
+	body.classList.add("fixed");
+};
+// Event listener for opening the modal when the button is clicked
+openModalBtn.addEventListener("click", openModal);
+
+// Function for closing the modal
+const closeModal = function () {
+	modal.classList.add("hidden");
+	overlay.classList.add("hidden");
+	body.classList.remove("fixed");
+};
+
+// Event listeners for clicking the close button, clicking the overlay, or pressing Esc key
+closeModalBtn.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+	if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+		closeModal();
+	}
+});
